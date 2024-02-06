0a23f28fe7f729ece003f757a063eb75499da8ab
NykooX <nykoofy@proton.me>
2024-02-06 12:57:44 -0300

    :sparkles: initial commit

---
+++ src/main/java/com/github/gabriellicar/nlw_expert_java/NlwExpertJavaApplication.java
@@ -0,0 +1,13 @@
+package com.github.gabriellicar.nlw_expert_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NlwExpertJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NlwExpertJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/gabriellicar/nlw_expert_java/NlwExpertJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.gabriellicar.nlw_expert_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NlwExpertJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
